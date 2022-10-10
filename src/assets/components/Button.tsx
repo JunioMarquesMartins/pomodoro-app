1257db8a7b19ed6cf3436101f63c524c35003b4f
Junio MM <info@juniomarques.com>
2022-10-10 23:25:50 +0200

    test

--- src/assets/components/Button.tsx
+++ src/assets/components/Button.tsx
@@ -1,9 +1,11 @@
 import { ButtonContainer, ButtonVariant } from "./Button.styles";
 
 interface ButtonProps {
+    
     variant?: ButtonVariant;
+
 }
 
 export function Button ({ variant = 'primary' }: ButtonProps ) {
     return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
+} 
