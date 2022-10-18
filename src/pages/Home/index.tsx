729b3c0caad03edf231723917554b0f9ad9c3a52
Junio MM <info@juniomarques.com>
2022-10-18 23:06:39 +0200

    add step in field

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">I will work on</label>
-          <TaskInput id="task" placeholder="Project Name" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Project Name"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+            <option value="Project 4" />
+          </datalist>
 
           <label htmlFor="">during</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
